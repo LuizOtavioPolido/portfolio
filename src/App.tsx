7bf2689e183c15c3883a179d5aba06ee7d3519c8
LuizOtavioPolido <polido.luiz@outlook.com>
2024-04-22 23:03:02 -0300

    dark mode implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
+import { ConfigProvider, theme, Button, Card, Flex, Layout } from 'antd'
+import { Content, Footer, Header } from 'antd/es/layout/layout'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FiMoon, FiSun } from 'react-icons/fi'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const { defaultAlgorithm, darkAlgorithm } = theme
+    const [isDarkMode, setIsDarkMode] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function renderContent() {
+        return (
+            <ConfigProvider
+                direction="ltr"
+                theme={{
+                    algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+                    components: {
+                        Layout: {
+                            headerBg: isDarkMode ? '#001529' : '#d9d9d9',
+                            footerBg: isDarkMode ? '#001529' : '#d9d9d9'
+                        }
+                    }
+                }}
+                
+            >
+                <Layout style={{height: '100%'}}> 
+                    <Header />
+                    <Content style={{height: '100%'}}>
+                    <div style={{height: '100%'}}>
+                    <h1>FALA GALERAAA</h1>
+                    <Button
+                        id="button-test"
+                        onClick={() => setIsDarkMode(!isDarkMode)}
+                    >
+                        {isDarkMode ? <FiSun /> : <FiMoon />}
+                    </Button>
+                    </div>
+                    </Content>
+                    <Footer style={{display:'flex', justifyContent: 'center'}}>Made by Luizão</Footer>
+                </Layout>
+            </ConfigProvider>
+        )
+    }
+
+    return renderContent()
 }
-
-export default App
